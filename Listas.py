709854a426514bf23449706332f173ead52583ab
Clauciofds <115019744+Clauciofds@users.noreply.github.com>
2023-02-08 12:55:55 -0300

    mudanca

--- Listas.py
+++ Listas.py
@@ -1,4 +1,5 @@
 # O CÓDIGO ABAIXO FOI ESCRITO EM UM IDE E NÃO O GOOGLE COLAB
+import numpy as numpy
 
 # Documentação relevante: https://docs.python.org/pt-br/3/tutorial/datastructures.html
 
@@ -196,3 +197,228 @@ def deposita(conta):
 # enta evocamos a nova função e atribuimos a conta um novo valor
 conta_do_gui = deposita(conta_do_gui)
 print('linha 189:', conta_do_gui)
+
+guilherme = ('Guilherme', 37, 1983)
+daniela = ('Daniela', 25, 1987)
+
+print('') # criando lista com os elementos sendo tuplas
+usuarios = [guilherme, daniela]
+print('linha 200:', usuarios)
+
+print('') # com isso podemos adiciona tuplas na nosse lista de usuários
+usuarios.append(('Paulo', 34, 1985))
+print('linha 207:', usuarios)
+
+conta_do_gui = ContaCorrente(15)
+conta_do_gui.deposita(500)
+conta_da_Dani = ContaCorrente(2345)
+conta_da_Dani.deposita(1000)
+
+print('') # Agora possuimos dois objentos diferentes de tuplas
+contas = (conta_do_gui, conta_da_Dani)
+print(contas)
+
+print('') # assim quando tentamos alterar um elemento, que nesse caso é um tupla não iremos
+          # consigir, garantindo a imutabilidade do nosso objeto
+# contas.append(3435)
+# print(contas)
+
+print('') # imprimindo nos lista de tuples
+for conta in contas:
+    print('linha 225:', conta)
+
+# <<<HERANÇA E POLIMORFISMO>>>
+
+from abc import ABCMeta, abstractmethod
+class Conta:
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    @abstractmethod
+    def passa_o_mes(self):
+        pass
+
+    def __str__(self):
+        return '[>>Codigo {} Saldo {}<<]'.format(self._codigo, self._saldo)
+
+print('') # Criando um conta com as classes
+print('linha 242:', Conta(88))
+
+print('') # usanda o método de herança para manipular nossos elementos
+class ContaCorrente(Conta):
+    def passa_o_mes(self):
+        self._saldo -= 2
+
+class ContaPoupanca(Conta):
+    def passa_o_mes(self):
+        self._saldo *= 1.01
+        self._saldo -= 3
+
+conta16 = ContaCorrente(16)
+conta16.deposita(1000)
+conta16.passa_o_mes()
+print('linha 245:', conta16)
+
+print('') #
+conta16 = ContaCorrente(16)
+conta16.deposita(1000)
+conta17 = ContaPoupanca(17)
+conta17.deposita(2500)
+contas = [conta16, conta17]
+
+for conta in contas:
+    conta.passa_o_mes() # duck tuping
+    print('linha 260:', conta)
+
+import array as arr
+
+arr.array('d', [1, 3.5 ])
+
+print('') # otimizando as funções de listagem com numpy
+import numpy as np
+
+numeros = np.array([1, 3.5])
+print('linha 276:', numeros)
+
+numeros += 3
+print('linha 282:', numeros)
+
+print('') # Neste caso estamos tentando criar uma nova instância, mas com uma impletação na classe
+          # mãe tornando os métodos abstratos, isso forca o erro e a indetificação do mesmo.
+          # Agora vou rodar o código, interpretou e vou tentar criar uma conta
+          # normal. Já deu erro, porque a classe Conta virou uma classe abstrata. Ela tem um
+          # método abstrato e não pode ser instanciada, porque ainda não implementaram todos
+          # os métodos abstratos, o método passa_o_mês().
+class ContaInvestimento(Conta):
+    pass
+
+ContaInvestimento(762)
+print(ContaInvestimento)
+
+print('') # Criando um nova classe para conta salário
+class ContaSalario:
+
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+
+    def deposita(self, valor):
+        self._saldo += valor
+
+    def __str__(self):
+        return '[>>>Código {} - Saldo {}<<<]'.format(self._codigo, self._saldo)
+
+conta1 = ContaSalario(37)
+print('linha 315:', conta1)
+
+conta2 = ContaSalario(37)
+print('linha 319:', conta2)
+
+print('') # comparando contas Salário. Nesta comparação estamos verificando dois objetos distintos na
+          # memória
+print('linha 321: A conta1 é igual a conta2?', conta1 == conta2)
+
+print('') # Criando um nova classe para conta salário, agora com a implementação da funçao de
+          # comparações nos elementos específico nos objetos
+class ContaSalario:
+
+    def __init__(self, codigo):
+        self._codigo = codigo
+        self._saldo = 0
+
+    def __eq__(self, outro):
+        return self._codigo == outro._codigo
+
+    def __eq__(self, outro):
+        if type(outro) != ContaSalario:
+            return False
+    def deposita(self, valor):
+        self._saldo += valor
+
+    def __str__(self):
+        return '[>>>Código {} - Saldo {}<<<]'.format(self._codigo, self._saldo)
+
+conta1 = ContaSalario(37)
+conta2 = ContaCorrente(37)
+conta3 = ContaSalario(37)
+
+print('linha 349:', conta1 == conta3)
+
+print('linha 351:', conta1 == conta2)
+
+print('linha 353:', isinstance(ContaCorrente(34), ContaCorrente))
+
+print('linha 355:', isinstance(ContaCorrente(34), Conta))
+
+print('linha 357:', conta2 in [conta1])
+
+print('') # novas funcoes
+idades = [15, 87, 65, 56, 32, 49, 37, 49]
+
+for i in range(len(idades)): #lazy
+    print('linha 362:', i, idades[i])
+
+print('') # lazy
+print('linha 366:', enumerate(idades))
+
+print('') # 
+print('linha 369:', type(range(len(idades))))
+
+print('') # 
+print(list(range(len(idades)))) # forcei a geração dos valores
+
+print('') # 
+print(list(enumerate(idades)))
+
+print('') # 
+for valor in enumerate(idades):
+    print(valor)
+
+print('') # unpacking da nossa tupla
+for indice, idade in enumerate(idades):
+    print(indice, 'x', idade)
+
+print('') # 
+usuarios = (
+    ('Guilherme', 37, 1981),
+    ('Daniela', 31, 1987),
+    ('Paulo', 39,1979)
+)
+
+for nome, idade, nascimento in usuarios:
+    print(nome, '-- Idade:', idade)
+
+print('') # ja desempacotando, ignorando o resto
+for nome, _, _, in usuarios:
+    print(nome)
+
+print('') # 
+print('linha 400:', sorted(idades))
+
+print('') # 
+print('linha 403:', sorted(idades, reverse=True))
+
+print('') # 
+print('linha 406:', reversed(idades))
+
+print('') # 
+print('linha 409:', idades)
+
+print('') # 
+print('linha 412:', list(reversed(idades)))
+
+print('') # 
+idades.sort()
+print('linha 416:', idades)
+
+print('') # 
+print('linha 419:', list(reversed(sorted(idades))))
+
+print('') # 
+print('linha 422:', idades)
+
+print('') # 
